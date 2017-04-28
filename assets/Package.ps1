dcf5a6bdffa751d97a96fe4f5e64cd783e238a22
George Olson <georgeo@slalom.com>
2017-04-28 06:12:42 -0700

    no message

---
+++ assets/Package.ps1
@@ -0,0 +1,64 @@
+﻿<#
+.SYNOPSIS
+    Packages the Stacks - Functions packages.
+#>
+param (
+    $Configuration = "DEBUG",
+    $IncrementVersion = $true
+)
+
+function Increment-Version() {
+    $jsonpath = 'project.json'
+    $json = Get-Content -Raw -Path $jsonpath | ConvertFrom-Json
+    $versionString = $json.version
+    $patchInt = [convert]::ToInt32($versionString.Split(".")[2].Split("-")[0], 10)
+    [int]$incPatch = $patchInt + 1
+    $patchUpdate = $versionString.Split(".")[0] + "." + $versionString.Split(".")[1] + "." + ($incPatch -as [string]) + "-*"
+    $json.version = $patchUpdate
+    $json = ConvertTo-Json $json -Depth 100
+
+
+    $json = Format-Json $json    
+    $json | Out-File  -FilePath $jsonpath
+}
+
+
+function Format-Json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {
+  $indent = 0;
+  ($json -Split '\n' |
+    % {
+      if ($_ -match '[\}\]]') {
+        # This line contains  ] or }, decrement the indentation level
+        $indent--
+      }
+      $line = (' ' * $indent * 2) + $_.TrimStart().Replace(':  ', ': ')
+      if ($_ -match '[\{\[]') {
+        # This line contains [ or {, increment the indentation level
+        $indent++
+      }
+      $line
+  }) -Join "`n"
+}
+
+function Go ($Path) {
+    Push-Location $Path
+
+    Remove-Item .\Bin -Force -Recurse
+    if ($IncrementVersion) {
+        Increment-Version
+    }
+    dotnet build
+    dotnet pack --no-build --configuration $Configuration
+    copy .\bin\$Configuration\*.nupkg c:\nuget\
+
+    Pop-Location
+}
+
+Push-Location $PSScriptRoot
+
+Go ..\src\Slalom.Stacks.Functions
+
+Pop-Location
+
+
+
--- src/Slalom.Stacks.Functions/Function.cs
+++ src/Slalom.Stacks.Functions/Function.cs
@@ -1,47 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using Slalom.Stacks.Messaging;
-using Slalom.Stacks.Messaging.Registry;
+using Slalom.Stacks.Services;
+using Slalom.Stacks.Services.Messaging;
 using Slalom.Stacks.Validation;
 
 namespace Slalom.Stacks.Functions
 {
-    public class FunctionHost
+    public class Function
     {
+        public static Stack Stack;
+
         public static async Task<HttpResponseMessage> Run(HttpRequestMessage req)
         {
-            using (var stack = new Stack())
+            var path = req.RequestUri.PathAndQuery.Split('?')[0].Trim('/');
+            if (path.StartsWith("api"))
             {
-                var path = req.RequestUri.PathAndQuery.Split('?')[0].Trim('/');
-                if (path.StartsWith("api"))
+                path = path.Substring(3).Trim('/');
+            }
+            var registry = Stack.GetServices();
+            var endPoint = registry.Find(path);
+            if (endPoint != null)
+            {
+                using (var stream = new MemoryStream())
                 {
-                    path = path.Substring(3).Trim('/');
-                }
-                var registry = stack.GetServices();
-                var endPoint = registry.Find(path);
-                if (endPoint != null)
-                {
-                    using (var stream = new MemoryStream())
-                    {
-                        await req.Content.CopyToAsync(stream);
+                    await req.Content.CopyToAsync(stream);
 
-                        var content = Encoding.UTF8.GetString(stream.ToArray());
-                        if (string.IsNullOrWhiteSpace(content))
-                        {
-                            content = null;
-                        }
-                        var result = await stack.Send(path, content);
-                        return HandleResult(result);
+                    var content = Encoding.UTF8.GetString(stream.ToArray());
+                    if (string.IsNullOrWhiteSpace(content))
+                    {
+                        content = null;
                     }
+                    var result = await Stack.Send(path, content);
+                    return HandleResult(result);
                 }
             }
             return new HttpResponseMessage(HttpStatusCode.NotFound);
@@ -57,23 +53,20 @@ namespace Slalom.Stacks.Functions
             {
                 return Respond(result.ValidationErrors, HttpStatusCode.Unauthorized);
             }
-            else if (result.ValidationErrors.Any())
+            if (result.ValidationErrors.Any())
             {
                 return Respond(result.ValidationErrors, HttpStatusCode.Conflict);
             }
-            else if (!result.IsSuccessful)
+            if (!result.IsSuccessful)
             {
                 var message = "An unhandled exception was raised on the server.  Please try again.  " + result.CorrelationId;
                 return Respond(message, HttpStatusCode.InternalServerError);
             }
-            else if (result.Response != null)
+            if (result.Response != null)
             {
                 return Respond(result.Response, HttpStatusCode.OK);
             }
-            else
-            {
-                return new HttpResponseMessage(HttpStatusCode.NoContent);
-            }
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
         }
 
         private static HttpResponseMessage Respond(object content, HttpStatusCode statusCode)
